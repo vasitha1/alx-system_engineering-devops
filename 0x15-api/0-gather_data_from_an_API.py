c099b4340f47987a3c5eacef5cb05cbe4440eaad
vasitha1 <sulemfelsi@gmail.com>
2025-02-10 22:41:17 +0000

    Using a rest API to print info

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module receives information from an API and displays it
+"""
+
+
+import requests
+import sys
+
+
+def main():
+    """ Defines the function"""
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+         len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
+
+
+if __name__ = "__main__":
+    main()
