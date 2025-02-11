d2d4fcfd459c6fcde098088c9069fac11cc7b918
vasitha1 <sulemfelsi@gmail.com>
2025-02-11 01:21:25 +0000

    Fixing pycode style

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,11 +13,7 @@ def main():
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-          len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
-
+    username = user.get("username")
 
      with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
