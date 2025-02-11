6623294b8ebcafd840ac87ab91eb23d8fcca86a1
vasitha1 <sulemfelsi@gmail.com>
2025-02-11 01:34:38 +0000

    Fixing pycode style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,24 +3,25 @@
 This module receives information from an API and displays it
 """
 
-import csv
+import json
 import requests
 import sys
 
 
 def main():
     """ Defines the function"""
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csvfile:
-
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
 
 
 if __name__ == "__main__":
